b8a0d7438617ca46286b61a64f50aa60993c7537
un1uckyyy <sergeygetmanov05@mail.ru>
2025-03-28 15:15:58 +0300

    added debug env flag

--- sem2/project/app/core/config.py
+++ sem2/project/app/core/config.py
@@ -2,6 +2,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    debug: bool = False
+
     secret_key: str
     algorithm: str = "HS256"
     access_token_expire_minutes: int = 30
--- sem2/project/app/db/sqlite.py
+++ sem2/project/app/db/sqlite.py
@@ -4,11 +4,12 @@ from fastapi import Depends
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 
+from ..core.config import settings
 from ..models.base import Base
 
 DATABASE_URL = "sqlite:///sem2.db"
 
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(DATABASE_URL, echo=settings.debug)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
